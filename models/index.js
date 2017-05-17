848ef282af6b7b21329f337b8e04f88938e60822
paul <paul.christophe6@gmail.com>
2017-05-16 22:15:27 -0400

    connecting to db successfully

---
+++ models/index.js
@@ -0,0 +1,44 @@
+const fs        = require('fs');
+const path      = require('path');
+const Sequelize = require('sequelize');
+const basename  = path.basename(module.filename);
+const env       = process.env.NODE_ENV || 'development';
+const db        = {};
+
+const sequelize = new Sequelize(process.env.PGURL, {
+  dialect:  'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: true
+  }
+});
+
+// TEST CONNECTION
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(function (err) {
+    console.log('Unable to connect to the database:', err);
+  });
+fs
+  .readdirSync(__dirname)
+  .filter(function(file) {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(function(file) {
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(function(modelName) {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-// const models = require('../models');
+const models = require('../models');
 
 router.get('/', function(req, res, next) {
   res.json({ status: true, data: 'WELCOME TO THE API' });
