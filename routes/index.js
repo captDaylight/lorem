83e3fa9ee7db70a2642a8747ffc8da20fa590e4e
paul <paul.christophe6@gmail.com>
2017-07-01 17:21:53 -0400

    setting up html routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/:hash', function(req, res, next) {
+  res.render('index', { hash: req.params.hash });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('what');
+  res.render('index');
 });
+
 
 module.exports = router;
